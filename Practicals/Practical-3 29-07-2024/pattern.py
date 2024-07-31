7c97dfddfd89d9805d13ff82ca2d618b04a71809
pratham-sggs <2022bit045@sggs.ac.in>
2024-07-31 21:55:41 +0530

    Pr-3

--- Practicals/Practical-3 29-07-2024/pattern.py
+++ Practicals/Practical-3 29-07-2024/pattern.py
@@ -11,8 +11,30 @@ line = int(line);
 k = print_pattern(line);
 base_width=line;
 line_to_print_number = line+1;
-total_number_of_spaces = 3*line;
-number_of_stars = 0;
+total_number_of_spaces = 2*(line+2);
+number_of_spaces = 0;
+star = 0;
+number = " ";
 #to print the upper part without base
-for i in range (0,line*2) :
-    print();
+for i in range (1,line+2) :
+    print(" "*(total_number_of_spaces - i*2) + "*" + " " * (number_of_spaces) + str(number) + " " * (number_of_spaces) +"*" * (star));
+    if(i==1) :
+        star = star+1;
+        number_of_spaces = 1;
+    if(i>1) :
+        number_of_spaces = number_of_spaces +2;
+    if(i==(line)) :
+        number = line;
+# to PRINT LOWER PYRAMID
+if(line>1) :
+    number = " ";
+    total_number_of_spaces=2;
+    num=line-1;
+    number_of_spaces=3+(num*4);
+    for i in range (1,line) :
+        print(" "*(total_number_of_spaces + i*2) + "*" + " " * (number_of_spaces-4) + "*");
+        number_of_spaces=number_of_spaces-4
+#print base
+for i in range (0,line) :
+    total_number_of_spaces=5;
+    print("* "*(total_number_of_spaces+(2*(line-1))));
