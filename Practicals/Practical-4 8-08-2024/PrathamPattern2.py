63944ec7f2eee0e8b2ade0e43336d0bf8327f56d
pratham-sggs <2022bit045@sggs.ac.in>
2024-08-06 01:49:53 +0530

    pr-4

--- Practicals/Practical-4 8-08-2024/PrathamPattern2.py
+++ Practicals/Practical-4 8-08-2024/PrathamPattern2.py
@@ -1,30 +1,26 @@
 def print_pattern (line) :
     list=[]
     if(line<1) :
-        list.append("Invalid Number");
-        return list;
+        print("Invalid Number");
     else :
         z=int(line);
         if(z-line) :
-            list.append("Invalid Number");
-            return list;
+            print("Invalid Number Please Enter Valid Number");
         line=int(line)
         total_number_of_spaces = 2*(line+2);
         number_of_spaces = 0;
         star = 0;
         number = " ";
-        inc = 0;
         #to print the upper part without base
         for i in range (1,line+2) :
-            list.append(" "*(total_number_of_spaces - i*2) + "*" + " " * (number_of_spaces) + str(number) + " " * (number_of_spaces-inc) +"*" * (star));
+            print(" "*(total_number_of_spaces - i*2) + "+" + " " * (number_of_spaces) + str(number) + " " * (number_of_spaces) +"+" * (star));
             if(i==1) :
                 star = star+1;
                 number_of_spaces = 1;
             if(i>1) :
                 number_of_spaces = number_of_spaces +2;
             if(i==(line)) :
-                number = line;
-                inc = len(str(line))-1
+                number = "*";
         # to PRINT LOWER PYRAMID
         if(line>1) :
             number = " ";
@@ -32,14 +28,8 @@ def print_pattern (line) :
             num=line-1;
             number_of_spaces=3+(num*4);
             for i in range (1,line) :
-                list.append(" "*(total_number_of_spaces + i*2) + "*" + " " * (number_of_spaces-4) + "*");
+                print(" "*(total_number_of_spaces + i*2) + "+" + " " * (number_of_spaces-4) + "+");
                 number_of_spaces=number_of_spaces-4
-        #print base
-        for i in range (0,line) :
-            total_number_of_spaces=5;
-            list.append("* "*(total_number_of_spaces+(2*(line-1))));
-    return list;
+    print(" "*(4 +(line-1)*2) + "-")
           
-k = print_pattern(45.0000);
-for i in range(0,len(k)) :
-    print(k[i])
+k = print_pattern(10.0000);
--- Practicals/Practical-4 8-08-2024/patrn.py
+++ Practicals/Practical-4 8-08-2024/patrn.py
@@ -1,26 +1,23 @@
 def modulo (a,b) :
-    if b == 0 :
-        return "Enter Valid Number"
-    if a == 0 :
+    if(b == 0) :
+        return "Error Divisor can't be Zero!"
+    first_number = abs(a)
+    second_number = abs(b)
+    mod = 0;
+    sum = 0;
+    while (sum<first_number) : 
+        sum += second_number
+    if(sum==first_number) :
+            return 0
+    else :
+            mod = first_number-sum+second_number
+    if(a > 0 and b > 0) :
+        return mod;
+    elif(a>0 and b<0 ) :
+        return mod-second_number;
+    elif(a < 0 and b < 0) :
+        return mod - 2*mod;
+    elif(a<0 and b>0) :
+        return mod-second_number+(2*abs(mod-second_number))
+    else :
         return 0
-    if a > 0 :
-        sum = 0
-        while (sum<a) :
-            sum = sum + b;
-        if (sum>a):
-            return a-sum+b;
-        return a-sum;
-    if a < 0 :
-        a = str(a)
-        a =a[1:]
-        a = int(a)
-        while (a>0) :
-            a = a - b;
-        a = str(a)
-        a =a[1:]
-        a = int(a)
-        return a;
-
-
-k = modulo(16,7) 
-print(k)
