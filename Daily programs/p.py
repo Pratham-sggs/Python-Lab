0585554d4b7fc8e18957981cb696119de4a3b03b
pratham-sggs <2022bit045@sggs.ac.in>
2024-08-23 11:01:35 +0530

    Practical

---
+++ Daily programs/p.py
@@ -0,0 +1,13 @@
+def isPalindrome(s: str) -> bool:
+        string_after = ""
+        for i in s :
+            if ord(i) <= 122 and ord(i) >= 97 or ord(i) >= 65 and ord(i) <= 90 or ord(i) >= 48 and ord(i) <= 57 :
+                string_after = string_after + i.lower()
+        temp = reversed(string_after)
+        print("".join(temp))
+        if "".join(temp) == string_after :
+            return True
+        else :
+            return False
+
+isPalindrome(",,,,,,,,,,,,acva")
