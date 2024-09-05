8ac1598c9adf25cec65c9652f8527547677caf8d
pratham-sggs <2022bit045@sggs.ac.in>
2024-09-05 22:37:11 +0530

    Practical

---
+++ Daily programs/p.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+int main()
+{
+    int a,b,c;
+    char operation ;
+    printf("enter first number: ");
+    scanf("%d",&a);
+    printf("enter second number: ");
+    scanf("%d",&b);
+    printf("enter operation :");
+    scanf(" %c",&operation);
+    if(operation == '+'){
+    c=a+b    ;
+    }
+    else if (operation == '-'){
+        c=a-b;
+
+    }
+     else if (operation == '*'){
+        c=a*b;
+
+    } else if (operation == '/'){
+        c=a/b;
+
+    }
+    else {
+        printf("error");
+        return 0;
+    }
+    printf("result %d",c);
+
+}
+
