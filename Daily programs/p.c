dd3e1047f630178b77fc161d56306b22f230a692
pratham-sggs <2022bit045@sggs.ac.in>
2024-09-07 00:51:47 +0530

    Practical

--- Daily programs/p.c
+++ Daily programs/p.c
@@ -1,33 +1,29 @@
 #include <stdio.h>
-int main()
-{
-    int a,b,c;
-    char operation ;
-    printf("enter first number: ");
-    scanf("%d",&a);
-    printf("enter second number: ");
-    scanf("%d",&b);
-    printf("enter operation :");
-    scanf(" %c",&operation);
-    if(operation == '+'){
-    c=a+b    ;
-    }
-    else if (operation == '-'){
-        c=a-b;
-
-    }
-     else if (operation == '*'){
-        c=a*b;
-
-    } else if (operation == '/'){
-        c=a/b;
-
-    }
-    else {
-        printf("error");
-        return 0;
-    }
-    printf("result %d",c);
+int main(){
+int a,b,c;
+char operation;
+printf("enter first number");
+scanf("%d",&a);
+printf("enter second number");
+scanf("%d",&b);
+printf("enter operation");
+scanf(" %c",&operation);
+if (operation=='+'){
+    c=a+b;
+}
+else if (operation=='-'){
+    c=a-b;
 
 }
+else if (operation=='*'){
+    c=a*b;
+    
+}
+else if (operation=='/'){
+    c=a/b;
+    
+}
+else 
+printf ("error")
 
+}
