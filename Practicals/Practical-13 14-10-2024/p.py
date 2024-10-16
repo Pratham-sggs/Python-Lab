a4938328e4d142389364e3c4c568b5a03eaa4ac2
pratham-sggs <2022bit045@sggs.ac.in>
2024-10-17 01:12:09 +0530

    Practical 13

--- Practicals/Practical-13 14-10-2024/p.py
+++ Practicals/Practical-13 14-10-2024/p.py
@@ -1,1 +1,28 @@
-print("Pratham")
+def areSentencesSimilar( sentence1: str, sentence2: str) -> bool:
+        if len(sentence1) < len(sentence2) :
+            sentence1, sentence2 = sentence2, sentence1
+        if sentence1.startswith(sentence2) or sentence1.endswith(sentence2) :
+            return True
+        s = sentence1.split()
+        s1 = sentence2.split()
+        result = []
+        truth = True
+        start = []
+        end = []
+        for i in s : 
+            if i not in s1 :
+                result.append(i)
+                truth = False
+            else :
+                if truth :
+                    start.append(i)
+                else :
+                    end.append(i)
+        result = " ".join(start) + " " + " ".join(result) + " " + " ".join(end)
+        print(result)
+        print(sentence1)
+        return result == sentence1
+print(areSentencesSimilar("My name is Haley","My Haley"))
+print(areSentencesSimilar("of","A lot of words"))
+print(areSentencesSimilar("Eating right now","Eating"))
+print(areSentencesSimilar("UI wqhw Lf","ezzXqqEQcS"))
